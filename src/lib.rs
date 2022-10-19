6bc0b6ecef842f692e17d2773667ab0afa010611
Michael Huang <michaelh@casual.place>
2022-10-18 17:23:18 -0700

    Add functions to read inputs
    We can now use the file names we get from the argument to open
    and read the corresponding file. It also works with stdin.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use clap::Parser;
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Parser)]
-#[command(author, version, about)]
+#[command(
+    version,
+    about,
+    long_about = "Concatenate FILE(s) to standard output.
+With no FILE, or when FILE is -, read standard output"
+)]
 pub struct Cli {
-    #[arg(help = "File(s) to concatenate")]
+    #[arg(default_values = vec!["-"], help = "File(s) to concatenate")]
     files: Vec<String>,
 
     #[arg(short, long, help = "number all output lines")]
@@ -20,6 +28,25 @@ pub fn get_args() -> MyResult<Cli> {
 }
 
 pub fn run(cli: Cli) -> MyResult<()> {
-    dbg!(cli);
+    for filename in cli.files {
+        match open(&filename) {
+            Err(e) => eprintln!("Failed to open {}: {}", filename, e),
+            Ok(text) => read_text(text)?,
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn read_text(text: Box<dyn BufRead>) -> MyResult<()> {
+    for line in text.lines() {
+        println!("{}", line?);
+    }
     Ok(())
 }
